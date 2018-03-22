8b780cc9a887144a123ecc3b289d0b911f177c9d
achmadridho <Souleater999>
2018-03-22 14:17:33 +0700

    build

--- src/assets/js/setup.js
+++ src/assets/js/setup.js
@@ -1,4 +1,4 @@
-const globalURL= "http://localhost:8183";
+const globalURL= "http://167.205.7.233:8183";
 const globalSetup = {
     app_id:4501
 };
@@ -6,7 +6,7 @@ const restAPI = {
   signin:globalURL+"/users/signin",
   signup:globalURL+"/users/simple/signup",
   classList:globalURL+"/class/list",
-  uploadFile:"http://localhost:3077/files/upload/single",
+  uploadFile:"http://167.205.7.233:3077/files/upload/single",
   createmaerial:globalURL+"/material/create",
   updatematerial:globalURL+"/material/update",
   deletematerial:globalURL+"/material/delete",
